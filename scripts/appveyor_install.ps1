36f3c05a0e19db97bd7da74c8549eba9e67c0ecf
mondus <p.richmond@sheffield.ac.uk>
2017-01-02 19:59:00 +0000

    Update appveyor_install.ps1

--- scripts/appveyor_install.ps1
+++ scripts/appveyor_install.ps1
@@ -5,12 +5,14 @@ $env:CUDA_REPO_PKG="cuda_8.0.44_win10_network.exe"
 
 # Get network installer
 Write-Host 'Downloading CUDA Network Installer'
-Invoke-WebRequest $env:CUDA_REPO_PKG_LOCATION -OutFile $env:CUDA_REPO_PKG
+Invoke-WebRequest $env:CUDA_REPO_PKG_LOCATION -OutFile $env:CUDA_REPO_PKG | Out-Null
 Write-Host 'Downloading Complete'
   
 # Invoke silent install of CUDA compiler and runtime (via network installer)
-#& "$env:CUDA_REPO_PKG.exe -s compiler_8.0 | Out-Null"
-try {
+Write-Host 'Installing CUDA Compiler and Runtime'
+& "$env:CUDA_REPO_PKG.exe -s compiler_8.0 | Out-Null"
+Write-Host 'Installation Complete.'
+<# try {
     Write-Host 'Installing CUDA Compiler and Runtime'
     $cuda_install_proc = Start-Process -FilePath "$env:CUDA_REPO_PKG" -ArgumentList "/s compiler_8.0" -Wait -PassThru
     $proc1.waitForExit()
@@ -22,3 +24,4 @@ try {
     write-host '$_.Exception.GetType().FullName is' $_.Exception.GetType().FullName
     write-host '$_.Exception.Message is' $_.Exception.Message
 }
+#>
