fdf41c97306f04a04e58b1aab67ea46a7b107120
mondus <p.richmond@sheffield.ac.uk>
2017-01-03 13:47:49 +0000

    correct cuda installer specified

--- scripts/appveyor_install.ps1
+++ scripts/appveyor_install.ps1
@@ -1,6 +1,6 @@
 # Powershell script for installing CUDA on appveyor instances
 
-$env:CUDA_REPO_PKG_LOCATION="https://developer.nvidia.com/compute/cuda/8.0/prod/network_installers/cuda_8.0.44_win10_network-exe"
+$env:CUDA_REPO_PKG_LOCATION="https://developer.nvidia.com/compute/cuda/8.0/prod/network_installers/cuda_8.0.44_windows_network-exe"
 $env:CUDA_REPO_PKG="cuda_8.0.44_win10_network.exe"
 
 # Get network installer
@@ -10,11 +10,9 @@ Write-Host 'Downloading Complete'
   
 # Invoke silent install of CUDA compiler and runtime with Visual Studio integration (via network installer)
 Write-Host 'Installing CUDA Compiler and Runtime'
-$install_result = & .\$env:CUDA_REPO_PKG -s compiler_8.0 visual_studio_integration_8.0 | Out-String
-$nvcc_version = nvcc -V | Out-String 
-Write-Host "$install_result $nvcc_version"
+& .\$env:CUDA_REPO_PKG -s compiler_8.0 visual_studio_integration_8.0 | Out-Null
 Write-Host 'Installation Complete.'
 
-# List directory output
-$rules = ls "C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120\BuildCustomizations\" | Out-String
-Write-Host "$rules"
+# output nvcc version info
+$nvcc_version = nvcc -V | Out-String 
+Write-Host "nvcc - v $nvcc_version"
