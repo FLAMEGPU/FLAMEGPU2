f97d03f48f12926b320d04f71884e499f7e52f6c
Mozhgan K. Chimeh <mozhgan.kabiri@gmail.com>
2018-02-23 11:48:12 +0000

    revert changes to travis

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -12,10 +12,10 @@ apt-get -y update
 apt-get install libboost-dev
 
 # Install CUDA (see caffe example https://github.com/BVLC/caffe/blob/master/scripts/travis/install-deps.sh)
-#CUDA_REPO_PKG=cuda-repo-ubuntu1404_8.0.44-1_amd64.deb
-CUDA_REPO_PKG=cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
-#wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/$CUDA_REPO_PKG
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/$CUDA_REPO_PKG
+CUDA_REPO_PKG=cuda-repo-ubuntu1404_8.0.44-1_amd64.deb
+#CUDA_REPO_PKG=cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/$CUDA_REPO_PKG
+#wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/$CUDA_REPO_PKG
 dpkg -i $CUDA_REPO_PKG
 rm $CUDA_REPO_PKG
 
@@ -23,11 +23,12 @@ rm $CUDA_REPO_PKG
 apt-get -y update
 
 # install packages
-CUDA_PKG_VERSION="9-1"
-CUDA_VERSION="9.1"
+CUDA_PKG_VERSION="8-0"
+CUDA_VERSION="8.0"
 apt-get install -y --no-install-recommends \
   cuda-core-$CUDA_PKG_VERSION \
   cuda-cudart-dev-$CUDA_PKG_VERSION
+#apt-get --yes --force-yes install cuda
   
 # manually create CUDA symlink
 ln -s /usr/local/cuda-$CUDA_VERSION /usr/local/cuda
