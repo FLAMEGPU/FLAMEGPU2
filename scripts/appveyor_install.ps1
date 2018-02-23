9992f5ce194ba40fe4d136dd09ea68676cf56a25
Mozhgan K. Chimeh <mozhgan.kabiri@gmail.com>
2018-02-23 15:32:03 +0000

    changes to appveyor

--- scripts/appveyor_install.ps1
+++ scripts/appveyor_install.ps1
@@ -12,8 +12,15 @@ Write-Host 'Downloading Complete'
   
 # Invoke silent install of CUDA compiler and runtime with Visual Studio integration (via network installer)
 Write-Host 'Installing CUDA Compiler and Runtime'
-& .\$env:CUDA_REPO_PKG -s nvcc_9.1 visual_studio_integration_9.1 nvprof_9.1 memcheck_9.1 gpu-library-advisor_9.1 nvprune_9.1 cudart_9.1| Out-Null
+& .\$env:CUDA_REPO_PKG -s compiler_9.1 nvcc_9.1 Display.Driver visual_studio_integration_9.1 nvprof_9.1 memcheck_9.1 gpu-library-advisor_9.1 nvprune_9.1 cudart_9.1 cublas_9.1 cublas_dev_9.1 curand_9.1 curand_dev_9.1| Out-Null
 #& .\$env:CUDA_REPO_PKG -s compiler_8.0 visual_studio_integration_8.0 command_line_tools_8.0 cudart_8.0| Out-Null
 Write-Host 'Installation Complete.'
 
+
+if NOT EXIST "%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin\cudart64_91.dll" ( 
+echo "Failed to install CUDA"
+exit /B 1
+)
+
+nvcc -V
 # TODO: Test the install was successful
