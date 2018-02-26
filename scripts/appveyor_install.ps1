d15eff58630057640778665b330d14387199115e
Mozhgan K. Chimeh <mozhgan.kabiri@gmail.com>
2018-02-26 12:23:33 +0000

    changes to appveyor

--- scripts/appveyor_install.ps1
+++ scripts/appveyor_install.ps1
@@ -1,9 +1,9 @@
 # Powershell script for installing CUDA on appveyor instances
 
-#$env:CUDA_REPO_PKG_LOCATION="https://developer.nvidia.com/compute/cuda/8.0/prod/network_installers/cuda_8.0.44_windows_network-exe"
-$env:CUDA_REPO_PKG_LOCATION="https://developer.nvidia.com/compute/cuda/9.1/Prod/network_installers/cuda_9.1.85_win10_network"
-#$env:CUDA_REPO_PKG="cuda_8.0.44_win10_network.exe"
-$env:CUDA_REPO_PKG="cuda_9.1.85_win10_network.exe"
+$env:CUDA_REPO_PKG_LOCATION="https://developer.nvidia.com/compute/cuda/8.0/prod/network_installers/cuda_8.0.44_windows_network-exe"
+#$env:CUDA_REPO_PKG_LOCATION="https://developer.nvidia.com/compute/cuda/9.1/Prod/network_installers/cuda_9.1.85_win10_network"
+$env:CUDA_REPO_PKG="cuda_8.0.44_win10_network.exe"
+#$env:CUDA_REPO_PKG="cuda_9.1.85_win10_network.exe"
 
 # Get network installer
 Write-Host 'Downloading CUDA Network Installer'
@@ -12,8 +12,8 @@ Write-Host 'Downloading Complete'
   
 # Invoke silent install of CUDA compiler and runtime with Visual Studio integration (via network installer)
 Write-Host 'Installing CUDA Compiler and Runtime'
-& .\$env:CUDA_REPO_PKG -s compiler_9.1 nvcc_9.1 Display.Driver visual_studio_integration_9.1 nvprof_9.1 memcheck_9.1 gpu-library-advisor_9.1 nvprune_9.1 cudart_9.1 cublas_9.1 cublas_dev_9.1 curand_9.1 curand_dev_9.1| Out-Null
-#& .\$env:CUDA_REPO_PKG -s compiler_8.0 visual_studio_integration_8.0 command_line_tools_8.0 cudart_8.0| Out-Null
+#& .\$env:CUDA_REPO_PKG -s compiler_9.1 nvcc_9.1 Display.Driver visual_studio_integration_9.1 nvprof_9.1 memcheck_9.1 gpu-library-advisor_9.1 nvprune_9.1 cudart_9.1 cublas_9.1 cublas_dev_9.1 curand_9.1 curand_dev_9.1| Out-Null
+& .\$env:CUDA_REPO_PKG -s compiler_8.0 visual_studio_integration_8.0 command_line_tools_8.0 cudart_8.0| Out-Null
 Write-Host 'Installation Complete.'
 
 # TODO: Test the install was successful
