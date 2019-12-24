ef586656ee05f52968ddb89bb304526df40ac885
Robert Chisholm <R.Chisholm@sheffield.ac.uk>
2019-12-24 11:48:56 +0000

    Adds proper test for mandatory and optional brute force messaging.
    Created features test dir for this, as it crosses multiple classes, alternatively could move to CUDAMessage?

---
+++ tests/test_cases/features/test_brute_force_messages.cu
@@ -0,0 +1,137 @@
+#include "gtest/gtest.h"
+
+#include <random>
+
+#include "flamegpu/flame_api.h"
+#include "flamegpu/runtime/flamegpu_api.h"
+
+namespace test_message_brute_force {
+    const char *MODEL_NAME = "Model";
+    const char *AGENT_NAME = "Agent";
+    const char *MESSAGE_NAME = "Message";
+    const char *IN_FUNCTION_NAME = "InFunction";
+    const char *OUT_FUNCTION_NAME = "OutFunction";
+    const char *IN_LAYER_NAME = "InLayer";
+    const char *OUT_LAYER_NAME = "OutLayer";
+    const unsigned int AGENT_COUNT = 512;
+
+FLAMEGPU_AGENT_FUNCTION(OutFunction) {
+    FLAMEGPU->addMessage("x", FLAMEGPU->getVariable<int>("x"));
+    return ALIVE;
+}
+FLAMEGPU_AGENT_FUNCTION(OutFunction_Optional) {
+    const int x = FLAMEGPU->getVariable<int>("x");
+    if(x) FLAMEGPU->addMessage("x", x);
+    return ALIVE;
+}
+FLAMEGPU_AGENT_FUNCTION(InFunction) {
+    int sum = FLAMEGPU->getVariable<int>("sum");
+    int product = FLAMEGPU->getVariable<int>("product");
+    for (auto &message : FLAMEGPU->GetMessageIterator("Message")) {
+        const int x = message.getVariable<int>("x");
+        sum += x;
+        product *= x;
+    }
+    FLAMEGPU->setVariable<int>("sum", sum);
+    FLAMEGPU->setVariable<int>("product", product);
+    return ALIVE;
+}
+
+/**
+ * Test whether a group of agents can output unique messages which can then all be read back by the same (all) agents
+ */
+TEST(TestMessage_BruteForce, Mandatory1) {
+    ModelDescription m(MODEL_NAME);
+    MessageDescription &msg = m.newMessage(MESSAGE_NAME);
+    msg.newVariable<int>("x");
+    AgentDescription &a = m.newAgent(AGENT_NAME);
+    a.newVariable<int>("x");
+    a.newVariable<int>("sum");
+    a.newVariable<int>("product");
+    AgentFunctionDescription &fo = a.newFunction(OUT_FUNCTION_NAME, OutFunction);
+    fo.setMessageOutput(msg);
+    AgentFunctionDescription &fi = a.newFunction(IN_FUNCTION_NAME, InFunction);
+    fi.setMessageInput(msg);
+
+    std::default_random_engine rng;
+    std::uniform_int_distribution<int> dist(-3, 3);
+    AgentPopulation pop(a, (unsigned int)AGENT_COUNT);
+    int sum = 0;
+    int product = 1;
+    for (unsigned int i = 0; i < AGENT_COUNT; ++i) {
+        AgentInstance ai = pop.getNextInstance();
+        const int x = dist(rng);
+        sum += x;
+        product *= x;
+        ai.setVariable<int>("x", x);
+        ai.setVariable<int>("sum", 0);
+        ai.setVariable<int>("product", 1);
+    }
+    LayerDescription &lo = m.newLayer(OUT_LAYER_NAME);
+    lo.addAgentFunction(fo);
+    LayerDescription &li = m.newLayer(IN_LAYER_NAME);
+    li.addAgentFunction(fi);
+    CUDAAgentModel c(m);
+    c.SimulationConfig().steps = 1;
+    c.setPopulationData(pop);
+    c.simulate();
+    c.getPopulationData(pop);
+    // Validate each agent has same result
+    for (unsigned int i = 0; i < AGENT_COUNT; ++i) {
+        AgentInstance ai = pop.getInstanceAt(i);
+        ASSERT_EQ(ai.getVariable<int>("sum"), sum);
+        ASSERT_EQ(ai.getVariable<int>("product"), product);
+    }
+}
+
+
+/**
+ * Test whether a group of agents can optionally output unique messages which can then all be read back by the same agents
+ */
+TEST(TestMessage_BruteForce, Optional1) {
+    ModelDescription m(MODEL_NAME);
+    MessageDescription &msg = m.newMessage(MESSAGE_NAME);
+    msg.newVariable<int>("x");
+    AgentDescription &a = m.newAgent(AGENT_NAME);
+    a.newVariable<int>("x");
+    a.newVariable<int>("sum");
+    a.newVariable<int>("product");
+    AgentFunctionDescription &fo = a.newFunction(OUT_FUNCTION_NAME, OutFunction);
+    fo.setMessageOutput(msg);
+    AgentFunctionDescription &fi = a.newFunction(IN_FUNCTION_NAME, InFunction);
+    fi.setMessageInput(msg);
+
+    std::default_random_engine rng;
+    std::uniform_int_distribution<int> dist(-3, 3);
+    AgentPopulation pop(a, (unsigned int)AGENT_COUNT);
+    int sum = 0;
+    int product = 1;
+    for (unsigned int i = 0; i < AGENT_COUNT; ++i) {
+        AgentInstance ai = pop.getNextInstance();
+        const int x = dist(rng);
+        if (x) {
+            sum += x;
+            product *= x;
+        }
+        ai.setVariable<int>("x", x);
+        ai.setVariable<int>("sum", 0);
+        ai.setVariable<int>("product", 1);
+    }
+    LayerDescription &lo = m.newLayer(OUT_LAYER_NAME);
+    lo.addAgentFunction(fo);
+    LayerDescription &li = m.newLayer(IN_LAYER_NAME);
+    li.addAgentFunction(fi);
+    CUDAAgentModel c(m);
+    c.SimulationConfig().steps = 1;
+    c.setPopulationData(pop);
+    c.simulate();
+    c.getPopulationData(pop);
+    // Validate each agent has same result
+    for (unsigned int i = 0; i < AGENT_COUNT; ++i) {
+        AgentInstance ai = pop.getInstanceAt(i);
+        ASSERT_EQ(ai.getVariable<int>("sum"), sum);
+        ASSERT_EQ(ai.getVariable<int>("product"), product);
+    }
+}
+
+}  // namespace test_message_brute_force
