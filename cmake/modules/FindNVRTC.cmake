5dec605330f3e77802dca9ae79b38ff8d6cf7f3a
Peter Heywood <peethwd@gmail.com>
2020-05-06 15:43:40 +0100

    Improve finding of nvrtc/nvtx via cmake
    Also formats those files

--- cmake/modules/FindNVRTC.cmake
+++ cmake/modules/FindNVRTC.cmake
@@ -11,33 +11,41 @@
 #    NVRTC_FOUND
 #    NVRTC_INCLUDE_DIRS
 #    NVRTC_LIBRARIES
+#
+# Manually specify NVRTC paths via -DNVRTC_ROOT=/path/to/cuda/install/location
 
 # CMake Native CUDA support doesn't provide the raw directory, only include
-# Note that CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES and CMAKE_CUDA_IMPLICIT_LINK_DIRECTORIES are not set for visual studio generators for CMAKE < 3.17, so this relies on searching the users PATH.
+# Note that CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES and CMAKE_CUDA_IMPLICIT_LINK_DIRECTORIES are not set for visual studio generators for CMAKE < 3.17, so this relies on searching the users HINTS/PATH.
+
+# Search Path heirarchy order is <PackageName>_ROOT >  NVRTC_INCLUDE_DIRS / NVRTC_LIBRARIES > cmake environment variables > HINTS > system environment variables > platform files for current system > PATHS
 
 # Attempt to find nvToolsExt.h containing directory
 find_path(NVRTC_INCLUDE_DIRS
-	NAMES
-		nvrtc.h
+    NAMES
+        nvrtc.h
+    HINTS
+        ${CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES}
     PATHS
         ${CMAKE_CUDA_TOOLKIT_INCLUDE_DIRECTORIES}
     PATH_SUFFIXES
-		include
-	)
+        include
+    )
 
 # Find the directory containing the dynamic library
 find_library(NVRTC_LIBRARIES
-	NAMES 
-		libnvrtc.so
-		nvrtc
-		nvrtc.lib 
-	PATHS 
-		${CMAKE_CUDA_IMPLICIT_LINK_DIRECTORIES}
-	PATH_SUFFIXES
-		lib
-		lib64
-		lib/Win32
-		lib/x64
+    NAMES 
+        libnvrtc.so
+        nvrtc
+        nvrtc.lib 
+    HINTS 
+        ${CMAKE_CUDA_IMPLICIT_LINK_DIRECTORIES}
+    PATHS
+        ${CMAKE_CUDA_IMPLICIT_LINK_DIRECTORIES}
+    PATH_SUFFIXES
+        lib
+        lib64
+        lib/Win32
+        lib/x64
 )
 
 # Apply standard cmake find package rules / variables. I.e. QUIET, NVRTC_FOUND etc.
