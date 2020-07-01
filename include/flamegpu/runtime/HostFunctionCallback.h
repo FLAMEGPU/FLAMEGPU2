e80049ac7717af6af8965812fb105c9f681fbc34
Peter Heywood <peethwd@gmail.com>
2020-07-01 15:45:03 +0100

    Other minor swig lint fixes

--- include/flamegpu/runtime/HostFunctionCallback.h
+++ include/flamegpu/runtime/HostFunctionCallback.h
@@ -1,14 +1,14 @@
 #ifndef INCLUDE_FLAMEGPU_RUNTIME_HOSTFUNCTIONCALLBACK_H_
 #define INCLUDE_FLAMEGPU_RUNTIME_HOSTFUNCTIONCALLBACK_H_
 
-//class FLAMEGPU_HOST_API;
+// class FLAMEGPU_HOST_API;
 
 class HostFunctionCallback {
   /**
    * Virtual Callback class which allows a user to create a callback function to be used as a host function in either step, exit or init functions.
    * This is mostly required to allow swig wrapping of host functions in a target language
    */
-  public:
+ public:
     virtual void run(FLAMEGPU_HOST_API*) = 0;
     virtual ~HostFunctionCallback() {}
 };
