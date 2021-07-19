23319f4fc4486f56a1460281b1b8ea38fa8c9cc7
Peter Heywood <peethwd@gmail.com>
2021-07-19 17:41:53 +0100

    Improve CMake for the test_dev target
    This renames the option TEST_DEV_MODE to BUILD_TESTS_DEV to match BUILD_TESTS
    
    BUILD_TESTS_DEV is independant from BUILD_TESTS, so tests_dev can be configured without tests
    
    Googletest fetching CMake is split out from tests/CMakeLists.txt to cmake/googletest.cmake
    
    Googletest version bumped to release-1.11.0 from a specific commit between 1.10.0 and 1.11.0
    
    Closes #580

---
+++ cmake/googletest.cmake
@@ -0,0 +1,29 @@
+##############
+# GOOGLETEST #
+##############
+
+set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/modules/ ${CMAKE_MODULE_PATH})
+include(FetchContent)
+cmake_policy(SET CMP0079 NEW)
+
+# Googltest newer than 389cb68b87193358358ae87cc56d257fd0d80189 (included in release-1.11.0) or newer is required for CMake >= 3.19
+FetchContent_Declare(
+  googletest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG        release-1.11.0
+)
+
+FetchContent_GetProperties(googletest)
+if(NOT googletest_POPULATED)
+    FetchContent_Populate(googletest)
+    # Suppress installation target, as this makes a warning
+    set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
+    mark_as_advanced(FORCE INSTALL_GTEST)
+    mark_as_advanced(FORCE BUILD_GMOCK)
+    # Prevent overriding the parent project's compiler/linker
+    # settings on Windows
+    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+    add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR} EXCLUDE_FROM_ALL)
+    CMAKE_SET_TARGET_FOLDER("gtest" "Tests/Dependencies")
+endif()
+
