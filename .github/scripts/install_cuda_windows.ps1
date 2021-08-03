1ba0a93805e7a53ee1027142d9bb7ad7af67bbf9
Peter Heywood <peethwd@gmail.com>
2021-08-03 10:33:23 +0100

    Make windows CI installs aware of CUDA 11.4.1 patch release

--- .github/scripts/install_cuda_windows.ps1
+++ .github/scripts/install_cuda_windows.ps1
@@ -27,6 +27,7 @@ $CUDA_KNOWN_URLS = @{
     "11.3.0" = "https://developer.download.nvidia.com/compute/cuda/11.3.0/network_installers/cuda_11.3.0_win10_network.exe"
     "11.3.1" = "https://developer.download.nvidia.com/compute/cuda/11.3.1/network_installers/cuda_11.3.1_win10_network.exe"
     "11.4.0" = "https://developer.download.nvidia.com/compute/cuda/11.4.0/network_installers/cuda_11.4.0_win10_network.exe"
+    "11.4.1" = "https://developer.download.nvidia.com/compute/cuda/11.4.1/network_installers/cuda_11.4.1_win10_network.exe"
 }
 
 # @todo - change this to be based on _MSC_VER intead, or invert it to be CUDA keyed instead?
