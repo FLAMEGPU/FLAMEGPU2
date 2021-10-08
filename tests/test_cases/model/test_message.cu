53b417c7021f7aa44e8060ffbfdbe143ee22e6d4
Peter Heywood <peethwd@gmail.com>
2021-10-08 12:36:58 +0100

    Add missing message getVariableLength method, for checking array messages work correctly

--- tests/test_cases/model/test_message.cu
+++ tests/test_cases/model/test_message.cu
@@ -30,8 +30,10 @@ TEST(MessageDescriptionTest, variables) {
     // Returned variable data is same
     EXPECT_EQ(sizeof(float), m.getVariableSize(VARIABLE_NAME1));
     EXPECT_EQ(std::type_index(typeid(float)), m.getVariableType(VARIABLE_NAME1));
+    EXPECT_EQ(1, m.getVariableLength(VARIABLE_NAME1));
     EXPECT_EQ(sizeof(int16_t), m.getVariableSize(VARIABLE_NAME2));
     EXPECT_EQ(std::type_index(typeid(int16_t)), m.getVariableType(VARIABLE_NAME2));
+    EXPECT_EQ(1, m.getVariableLength(VARIABLE_NAME2));
 }
 TEST(MessageDescriptionTest, variables_array) {
     ModelDescription _m(MODEL_NAME);
@@ -39,14 +41,14 @@ TEST(MessageDescriptionTest, variables_array) {
     EXPECT_FALSE(m.hasVariable(VARIABLE_NAME1));
     EXPECT_FALSE(m.hasVariable(VARIABLE_NAME2));
     EXPECT_EQ(m.getVariablesCount(), 0u);
-    m.newVariable<float>(VARIABLE_NAME1);
+    m.newVariable<float, 2>(VARIABLE_NAME1);
     EXPECT_EQ(m.getVariablesCount(), 1u);
-    m.newVariable<int16_t>(VARIABLE_NAME3);
+    m.newVariable<int16_t, 2>(VARIABLE_NAME3);
     EXPECT_EQ(m.getVariablesCount(), 2u);
-    m.newVariable<int16_t>(VARIABLE_NAME2);
+    m.newVariable<int16_t, 2>(VARIABLE_NAME2);
     EXPECT_EQ(m.getVariablesCount(), 3u);
     // Cannot create variable with same name
-    EXPECT_THROW(m.newVariable<int64_t>(VARIABLE_NAME1), exception::InvalidMessageVar);
+    EXPECT_THROW((m.newVariable<int64_t, 2>(VARIABLE_NAME1)), exception::InvalidMessageVar);
     // Cannot create array of length 0 (disabled, blocked at compilation with static_assert)
     // auto newVarArray0 = &MessageDescription::newVariable<int64_t, 0>;  // Use function ptr, can't do more than 1 template arg inside macro
     // EXPECT_THROW((m.*newVarArray0)(VARIABLE_NAME4), exception::InvalidMessageVar);
@@ -58,6 +60,8 @@ TEST(MessageDescriptionTest, variables_array) {
     EXPECT_EQ(sizeof(int16_t), m.getVariableSize(VARIABLE_NAME2));
     EXPECT_EQ(std::type_index(typeid(float)), m.getVariableType(VARIABLE_NAME1));
     EXPECT_EQ(std::type_index(typeid(int16_t)), m.getVariableType(VARIABLE_NAME2));
+    EXPECT_EQ(2, m.getVariableLength(VARIABLE_NAME1));
+    EXPECT_EQ(2, m.getVariableLength(VARIABLE_NAME2));
 }
 
 FLAMEGPU_AGENT_FUNCTION(NoInput, MessageNone, MessageSpatial3D) {
