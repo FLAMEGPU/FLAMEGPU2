e8e4bdc6f18dae7331c9d0e735061aa51e0bf60b
Robert Chisholm <robadob@robadob.org>
2022-02-22 13:06:21 +0000

    Add support for automatically sorting agents with xy and xyz location vars.
    Also added a bit of extra type checking and cuda error checking.

--- tests/test_cases/runtime/test_spatial_agent_sort.cu
+++ tests/test_cases/runtime/test_spatial_agent_sort.cu
@@ -10,7 +10,10 @@ namespace test_spatial_agent_sort {
 const unsigned int AGENT_COUNT = 4;
 
 // Function doesn't need to do anything, just needs to use spatial messaging
-FLAMEGPU_AGENT_FUNCTION(dummySpatialFunc, MessageSpatial3D, MessageNone) {
+FLAMEGPU_AGENT_FUNCTION(dummySpatialFunc_3D, MessageSpatial3D, MessageNone) {
+    return ALIVE;
+}
+FLAMEGPU_AGENT_FUNCTION(dummySpatialFunc_2D, MessageSpatial2D, MessageNone) {
     return ALIVE;
 }
 
@@ -28,7 +31,7 @@ TEST(AutomaticSpatialAgentSort, SortingDisabled) {
     locationMessage.setMin(-5, -5, -5);
     locationMessage.setMax(5, 5, 5);
     locationMessage.setRadius(0.2f);
-    AgentFunctionDescription& dummyFunc = agent.newFunction("dummySpatialFunc", dummySpatialFunc);
+    AgentFunctionDescription& dummyFunc = agent.newFunction("dummySpatialFunc", dummySpatialFunc_3D);
     dummyFunc.setMessageInput("location");
     LayerDescription& layer = model.newLayer();
     layer.addAgentFunction(dummyFunc);
@@ -73,7 +76,7 @@ TEST(AutomaticSpatialAgentSort, SortEveryStep) {
     locationMessage.setMin(-5, -5, -5);
     locationMessage.setMax(5, 5, 5);
     locationMessage.setRadius(0.2f);
-    AgentFunctionDescription& dummyFunc = agent.newFunction("dummySpatialFunc", dummySpatialFunc);
+    AgentFunctionDescription& dummyFunc = agent.newFunction("dummySpatialFunc", dummySpatialFunc_3D);
     dummyFunc.setMessageInput("location");
     LayerDescription& layer = model.newLayer();
     layer.addAgentFunction(dummyFunc);
@@ -102,6 +105,86 @@ TEST(AutomaticSpatialAgentSort, SortEveryStep) {
         finalOrder.push_back(instance.getVariable<int>("initial_order"));
     }
     std::vector<int> expectedResult {3, 2, 1, 0};
+    EXPECT_EQ(expectedResult, finalOrder);
+}
+
+// Initialises a reverse-sorted population and checks that it is correctly sorted after one step
+TEST(AutomaticSpatialAgentSort, SortEveryStep_vec2) {
+    // Define model
+    ModelDescription model("model");
+    AgentDescription& agent = model.newAgent("agent");
+    agent.newVariable<int>("initial_order");
+    agent.newVariable<float, 2>("xy");
+    MessageSpatial2D::Description& locationMessage = model.newMessage<MessageSpatial2D>("location");
+    locationMessage.setMin(-5, -5);
+    locationMessage.setMax(5, 5);
+    locationMessage.setRadius(0.2f);
+    AgentFunctionDescription& dummyFunc = agent.newFunction("dummySpatialFunc", dummySpatialFunc_2D);
+    dummyFunc.setMessageInput("location");
+    LayerDescription& layer = model.newLayer();
+    layer.addAgentFunction(dummyFunc);
+
+    // Init pop
+    AgentVector pop(agent, AGENT_COUNT);
+    for (int i = 0; i < static_cast<int>(AGENT_COUNT); i++) {
+        AgentVector::Agent instance = pop[i];
+        instance.setVariable<int>("initial_order", i);
+        instance.setVariable<float, 2>("xy", { static_cast<float>(-i), static_cast<float>(-i)});
+    }
+
+    // Setup Model
+    CUDASimulation cudaSimulation(model);
+    cudaSimulation.setPopulationData(pop);
+
+    // Execute step fn
+    cudaSimulation.step();
+
+    // Check results
+    cudaSimulation.getPopulationData(pop);
+    std::vector<int> finalOrder;
+    for (AgentVector::Agent instance : pop) {
+        finalOrder.push_back(instance.getVariable<int>("initial_order"));
+    }
+    std::vector<int> expectedResult{ 3, 2, 1, 0 };
+    EXPECT_EQ(expectedResult, finalOrder);
+}
+TEST(AutomaticSpatialAgentSort, SortEveryStep_vec3) {
+    // Define model
+    ModelDescription model("model");
+    AgentDescription& agent = model.newAgent("agent");
+    agent.newVariable<int>("initial_order");
+    agent.newVariable<float, 3>("xyz");
+    MessageSpatial3D::Description& locationMessage = model.newMessage<MessageSpatial3D>("location");
+    locationMessage.setMin(-5, -5, -5);
+    locationMessage.setMax(5, 5, 5);
+    locationMessage.setRadius(0.2f);
+    AgentFunctionDescription& dummyFunc = agent.newFunction("dummySpatialFunc", dummySpatialFunc_3D);
+    dummyFunc.setMessageInput("location");
+    LayerDescription& layer = model.newLayer();
+    layer.addAgentFunction(dummyFunc);
+
+    // Init pop
+    AgentVector pop(agent, AGENT_COUNT);
+    for (int i = 0; i < static_cast<int>(AGENT_COUNT); i++) {
+        AgentVector::Agent instance = pop[i];
+        instance.setVariable<int>("initial_order", i);
+        instance.setVariable<float, 3>("xyz", {static_cast<float>(-i), static_cast<float>(-i), static_cast<float>(-i) });
+    }
+
+    // Setup Model
+    CUDASimulation cudaSimulation(model);
+    cudaSimulation.setPopulationData(pop);
+
+    // Execute step fn
+    cudaSimulation.step();
+
+    // Check results
+    cudaSimulation.getPopulationData(pop);
+    std::vector<int> finalOrder;
+    for (AgentVector::Agent instance : pop) {
+        finalOrder.push_back(instance.getVariable<int>("initial_order"));
+    }
+    std::vector<int> expectedResult{ 3, 2, 1, 0 };
     EXPECT_EQ(expectedResult, finalOrder);
 }
 }  // namespace test_spatial_agent_sort
