510d854b6694890a3de16608a5b9e6697968821f
Robert Chisholm <robadob@robadob.org>
2022-02-25 11:02:24 +0000

    Fix Python tests.
    Mostly recent changes to logging that weren't tested with Py.

--- include/flamegpu/sim/AgentLoggingConfig_Reductions.cuh
+++ include/flamegpu/sim/AgentLoggingConfig_Reductions.cuh
@@ -1,8 +1,6 @@
 #ifndef INCLUDE_FLAMEGPU_SIM_AGENTLOGGINGCONFIG_REDUCTIONS_CUH_
 #define INCLUDE_FLAMEGPU_SIM_AGENTLOGGINGCONFIG_REDUCTIONS_CUH_
 
-#include <functional>
-
 namespace flamegpu {
 namespace detail {
 
@@ -54,62 +52,6 @@ __device__ __forceinline__ OutT standard_deviation_subtract_mean_impl::unary_fun
 }
 
 }  // namespace detail
-
-/**
- * Template for converting a type to the most suitable type of the same format with greatest range
- * Useful when summing unknown values
- * e.g. sum_input_t<float>::result_t == double
- * e.g. sum_input_t<uint8_t>::result_t == uint64_t
- */
-template <typename T> struct sum_input_t;
-/**
- * @see sum_input_t
- */
-template <> struct sum_input_t<float> { typedef double result_t; };
-/**
- * @see sum_input_t
- */
-template <> struct sum_input_t<double> { typedef double result_t; };
-/**
- * @see sum_input_t
- */
-template <> struct sum_input_t<char> { typedef uint64_t result_t; };
-/**
- * @see sum_input_t
- */
-template <> struct sum_input_t<uint8_t> { typedef uint64_t result_t; };
-/**
- * @see sum_input_t
- */
-template <> struct sum_input_t<uint16_t> { typedef uint64_t result_t; };
-/**
- * @see sum_input_t
- */
-template <> struct sum_input_t<uint32_t> { typedef uint64_t result_t; };
-/**
- * @see sum_input_t
- */
-template <> struct sum_input_t<uint64_t> { typedef uint64_t result_t; };
-/**
- * @see sum_input_t
- */
-template <> struct sum_input_t<int8_t> { typedef int64_t result_t; };
-/**
- * @see sum_input_t
- */
-template <> struct sum_input_t<int16_t> { typedef int64_t result_t; };
-/**
- * @see sum_input_t
- */
-template <> struct sum_input_t<int32_t> { typedef int64_t result_t; };
-/**
- * @see sum_input_t
- */
-template <> struct sum_input_t<int64_t> { typedef int64_t result_t; };
-/**
- * @see sum_input_t
- */
-template <typename T> struct sum_input_t { typedef T result_t; };
 
 }  // namespace flamegpu
 
