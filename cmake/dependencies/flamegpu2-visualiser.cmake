a2c828567f81abb1ff17a7b53af5c9ccf36691b2
Robert Chisholm <robadob@robadob.org>
2023-02-24 11:29:14 +0000

    Update FLAMEGPU2-vis hash
    This pulls in a compilation fix for Tegra/Arm

--- cmake/dependencies/flamegpu2-visualiser.cmake
+++ cmake/dependencies/flamegpu2-visualiser.cmake
@@ -7,7 +7,7 @@ include(FetchContent)
 cmake_policy(SET CMP0079 NEW)
 
 # Set the visualiser repo and tag to use unless overridden by the user.
-set(DEFAULT_FLAMEGPU_VISUALISATION_GIT_VERSION "79e913be8c9c65011ee7d6b5c793e1940a696ce8")
+set(DEFAULT_FLAMEGPU_VISUALISATION_GIT_VERSION "35b428cfa2d380c516a14648484da24720350407")
 set(DEFAULT_FLAMEGPU_VISUALISATION_REPOSITORY "https://github.com/FLAMEGPU/FLAMEGPU2-visualiser.git")
 
 # Set a VISUSLAITION_ROOT cache entry so it is available in the GUI to override the location if required
