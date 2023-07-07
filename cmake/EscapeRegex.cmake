0fd37c6cc1af86944630a9d874ceda66a70692c9
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2023-07-07 09:54:04 +0100

    CMake: Escape user-provided regex patterns.
    Closes #1071

---
+++ cmake/EscapeRegex.cmake
@@ -0,0 +1,7 @@
+# Define a function for escaping regex chars from strings
+# This prevents regex failures when user provided paths contain regex special chars
+function(escape_regex IN_STRING OUT_VAR)
+    # https://gitlab.kitware.com/cmake/cmake/-/issues/18580#note_483128
+    string(REGEX REPLACE "([][+.*()^])" "\\\\\\1" OUT_STRING "${IN_STRING}")
+    set(${OUT_VAR} "${OUT_VAR}" PARENT_SCOPE)
+endfunction()
