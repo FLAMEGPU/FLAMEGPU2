1a56502732bcfcbbd22db919951eb8584666eae2
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2023-07-18 14:43:57 +0100

    Fix CMake's EscapeRegex fn
    Must have got broken when tidying up #1085 and missed in review.

--- cmake/EscapeRegex.cmake
+++ cmake/EscapeRegex.cmake
@@ -3,5 +3,5 @@
 function(escape_regex IN_STRING OUT_VAR)
     # https://gitlab.kitware.com/cmake/cmake/-/issues/18580#note_483128
     string(REGEX REPLACE "([][+.*()^])" "\\\\\\1" OUT_STRING "${IN_STRING}")
-    set(${OUT_VAR} "${OUT_VAR}" PARENT_SCOPE)
+    set(${OUT_VAR} "${OUT_STRING}" PARENT_SCOPE)
 endfunction()
