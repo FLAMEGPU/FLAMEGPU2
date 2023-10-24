4070c7835a7dd9bae16fcf6dcfa23ce12d14917f
Robert Chisholm <R.Chisholm@sheffield.ac.uk>
2023-10-24 15:06:32 +0100

    Find swig works now

--- swig/conda/build.sh
+++ swig/conda/build.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
+# Setup user config
 build_threads=${FLAMEGPU_CONDA_BUILD_THREADS:-1}
 [[ -z "$FLAMEGPU_CONDA_CUDA_ARCHITECTURES" ]] && build_arch="" || build_arch="-DCMAKE_CUDA_ARCHITECTURES=$FLAMEGPU_CONDA_CUDA_ARCHITECTURES"
 
+# Locate SWIG
+# (CMake can't auto find conda installed SWIG)
+swig_exe=$(find ${CONDA_PREFIX} -type f -name swig -print -quit)
+swig_dir=$(dirname $(find ${CONDA_PREFIX} -type f -name swig.swg -print -quit))
+if [[ ! -z "$swig_exe" ]] && [[ ! -z "$swig_dir" ]]; then
+swig_exe="-DSWIG_EXECUTABLE=$swig_exe"
+swig_dir="-DSWIG_DIR=$swig_dir"
+else
+swig_exe=""
+swig_dir=""
+fi
+echo "swig_exe: $swig_exe"
+echo "swig_dir: $swig_dir"
 # Setup python packages
 # Issues with CMake locating ones installed by conda
 
-pip install setuptools wheel build astpretty
+#pip install setuptools wheel build astpretty
 
 mkdir -p build && cd build
 
-#cmake .. -DCMAKE_BUILD_TYPE=Release -DFLAMEGPU_BUILD_PYTHON=ON -DFLAMEGPU_BUILD_PYTHON_CONDA=ON
-#temp faster build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DFLAMEGPU_BUILD_PYTHON=ON -DFLAMEGPU_BUILD_PYTHON_VENV=OFF -DFLAMEGPU_BUILD_PYTHON_CONDA=ON $build_arch
+# Configure CMake
+cmake .. -DCMAKE_BUILD_TYPE=Release -DFLAMEGPU_BUILD_PYTHON=ON -DFLAMEGPU_BUILD_PYTHON_VENV=OFF -DFLAMEGPU_BUILD_PYTHON_CONDA=ON $build_arch $swig_exe $swig_dir
 
-# Build python wheel
-cmake --build . --target pyflamegpu --parallel ${build_threads}
+# Build Python wheel
+#cmake --build . --target pyflamegpu --parallel ${build_threads}
 
 # Install built wheel
-pip install lib/Release/python/dist/pyflamegpu-2.0.0rc1+cuda122-cp38-cp38-linux_x86_64.whl
+#pip install lib/Release/python/dist/pyflamegpu-2.0.0rc1+cuda122-cp38-cp38-linux_x86_64.whl
 
 # Cleanup
 cd .
