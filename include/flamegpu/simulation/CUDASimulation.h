FLAMEGPU/FLAMEGPU2

commit 7a7944d0e3de43882b77e4f9f4dc8fda7b43b6c1

DeviceAPI::isAgent() DeviceAPI::isState() (#1116)
* DeviceAPI::isAgent() DeviceAPI::isState()

With new C/Python tests

Closes #1108

Currently only RTC implemented with Python test.

* add C test, it's a clone of Python test.

* Agent python

Lint fix for prev C test.

* Unify the implementation of dstrcmp()

* Fix doc ci error

* C implementation and test fix

* Fix docs CI

* Place dstring.h inside namespace.

include/flamegpu/simulation/CUDASimulation.h
@@ -20,6 +20,7 @@
 #include "flamegpu/runtime/agent/HostNewAgentAPI.h"
 #include "flamegpu/simulation/detail/CUDAMacroEnvironment.h"
 #include "flamegpu/simulation/detail/EnvironmentManager.cuh"
+#include "flamegpu/simulation/detail/DeviceStrings.h"
 
 #ifdef FLAMEGPU_VISUALISATION
 #include "flamegpu/visualiser/ModelVis.h"
@@ -566,6 +567,10 @@ class CUDASimulation : public Simulation {
          * Provides buffers for device error checking
          */
         exception::DeviceExceptionManager exception;
+        /**
+         * Provides copies of strings (agent/state names) on device
+         */
+        detail::DeviceStrings strings;
 #endif
         explicit Singletons(const std::shared_ptr<detail::EnvironmentManager> &environment) : environment(environment) { }
     } * singletons;

include/flamegpu/runtime/AgentFunction.cuh
@@ -24,6 +24,8 @@ typedef void(AgentFunctionWrapper)(
 #endif
 #ifndef __CUDACC_RTC__
     const detail::curve::CurveTable *d_curve_table,
+    const char* d_agent_name,
+    const char* d_state_name,
     const char* d_env_buffer,
 #endif
     id_t *d_agent_output_nextID,
@@ -40,6 +42,8 @@ typedef void(AgentFunctionWrapper)(
  * Initialises FLAMEGPU_API instance
  * @param error_buffer Buffer used for detecting and reporting exception::DeviceErrors (flamegpu must be built with FLAMEGPU_SEATBELTS enabled for this to be used)
  * @param d_curve_table Pointer to curve hash table in device memory
+ * @param d_agent_name Pointer to agent name string
+ * @param d_state_name Pointer to agent state string
  * @param d_env_buffer Pointer to env buffer in device memory
  * @param d_agent_output_nextID If agent output is enabled, this points to a global memory src of the next suitable agent id, this will be atomically incremented at birth
  * @param popNo Total number of agents executing the function (number of threads launched)
@@ -60,6 +64,8 @@ __global__ void agent_function_wrapper(
 #endif
 #ifndef __CUDACC_RTC__
     const detail::curve::CurveTable* __restrict__ d_curve_table,
+    const char* d_agent_name,
+    const char* d_state_name,
     const char* d_env_buffer,
 #endif
     id_t *d_agent_output_nextID,
@@ -77,6 +83,8 @@ __global__ void agent_function_wrapper(
         sm()->device_exception = error_buffer;
 #endif
 #ifndef __CUDACC_RTC__
+        sm()->agent_name = d_agent_name;
+        sm()->state_name = d_state_name;
         sm()->env_buffer = d_env_buffer;
 #endif
     }

include/flamegpu/runtime/AgentFunctionCondition.cuh
@@ -19,6 +19,8 @@ typedef void(AgentFunctionConditionWrapper)(
 #endif
 #ifndef __CUDACC_RTC__
     const detail::curve::CurveTable* d_curve_table,
+    const char* d_agent_name,
+    const char* d_state_name,
     const char* d_env_buffer,
 #endif
     const unsigned int popNo,
@@ -30,6 +32,8 @@ typedef void(AgentFunctionConditionWrapper)(
  * Initialises FLAMEGPU_API instance
  * @param error_buffer Buffer used for detecting and reporting exception::DeviceErrors (flamegpu must be built with FLAMEGPU_SEATBELTS enabled for this to be used)
  * @param d_curve_table Pointer to curve hash table in device memory
+ * @param d_agent_name Pointer to agent name string
+ * @param d_state_name Pointer to agent state string
  * @param d_env_buffer Pointer to env buffer in device memory
  * @param popNo Total number of agents exeucting the function (number of threads launched)
  * @param d_rng Array of curand states for this kernel
@@ -44,6 +48,8 @@ __global__ void agent_function_condition_wrapper(
 #endif
 #ifndef __CUDACC_RTC__
     const detail::curve::CurveTable* __restrict__ d_curve_table,
+    const char* d_agent_name,
+    const char* d_state_name,
     const char* d_env_buffer,
 #endif
     const unsigned int popNo,
@@ -56,6 +62,8 @@ __global__ void agent_function_condition_wrapper(
         sm()->device_exception = error_buffer;
 #endif
 #ifndef __CUDACC_RTC__
+        sm()->agent_name = d_agent_name;
+        sm()->state_name = d_state_name;
         sm()->env_buffer = d_env_buffer;
 #endif
     }

include/flamegpu/runtime/DeviceAPI.cuh
@@ -129,6 +129,26 @@ class ReadOnlyDeviceAPI {
 #endif
         return blockIdx.x * blockDim.x + threadIdx.x;
     }
+    /**
+     * When passed an agent name, returns a boolean to confirm whether it matches the name of the current agent
+     *
+     * This function may be useful if an agent function is shared between multiple agents
+     *
+     * @note The performance of this function is unlikely to be cheap unless used as part of an RTC agent function.
+     */
+    __forceinline__ __device__ bool isAgent(const char* agent_name) {
+        return detail::curve::DeviceCurve::isAgent(agent_name);
+    }
+    /**
+     * When passed an agent state, returns a boolean to confirm whether it matches the name of the agent input state of the current agent function
+     *
+     * This function may be useful if an agent function is shared between multiple agent states
+     *
+     * @note The performance of this function is unlikely to be cheap unless used as part of an RTC agent function (whereby it can be processed at compile time).
+     */
+    __forceinline__ __device__ bool isState(const char* agent_state) {
+        return detail::curve::DeviceCurve::isState(agent_state);
+    }
 };
 
 /** @brief    A flame gpu api class for the device runtime only
@@ -335,6 +355,26 @@ class DeviceAPI {
         assert(gridDim.z == 1);
 #endif
         return blockIdx.x * blockDim.x + threadIdx.x;
+    }
+    /**
+     * When passed an agent name, returns a boolean to confirm whether it matches the name of the current agent
+     *
+     * This function may be useful if an agent function is shared between multiple agents
+     *
+     * @note The performance of this function is unlikely to be cheap unless used as part of an RTC agent function.
+     */
+    __forceinline__ __device__ bool isAgent(const char* agent_name) {
+        return detail::curve::DeviceCurve::isAgent(agent_name);
+    }
+    /**
+     * When passed an agent state, returns a boolean to confirm whether it matches the name of the agent input state of the current agent function
+     *
+     * This function may be useful if an agent function is shared between multiple agent states
+     *
+     * @note The performance of this function is unlikely to be cheap unless used as part of an RTC agent function (whereby it can be processed at compile time).
+     */
+    __forceinline__ __device__ bool isState(const char* agent_state) {
+        return detail::curve::DeviceCurve::isState(agent_state);
     }
 
     /**

include/flamegpu/runtime/detail/SharedBlock.h
@@ -22,6 +22,8 @@ struct SharedBlock {
     unsigned int curve_count[curve::Curve::MAX_VARIABLES];
 #endif
     const char* env_buffer;
+    const char* agent_name;
+    const char* state_name;
 #endif
 #if !defined(FLAMEGPU_SEATBELTS) || FLAMEGPU_SEATBELTS
     exception::DeviceExceptionBuffer *device_exception;

include/flamegpu/runtime/detail/curve/DeviceCurve.cuh
@@ -5,6 +5,7 @@
 #include "flamegpu/runtime/detail/curve/Curve.cuh"
 #include "flamegpu/exception/FLAMEGPUDeviceException_device.cuh"
 #include "flamegpu/detail/type_decode.h"
+#include "flamegpu/util/dstring.h"
 
 #ifdef FLAMEGPU_USE_GLM
 #ifdef __CUDACC__
@@ -244,6 +245,23 @@ class DeviceCurve {
      */
     template<typename T, unsigned int I = 1, unsigned int J = 1, unsigned int K = 1, unsigned int W = 1, unsigned int M>
     __device__ __forceinline__ static char *getEnvironmentMacroProperty(const char(&name)[M]);
+
+    /**
+     * When passed an agent name, returns a boolean to confirm whether it matches the name of the current agent
+     *
+     * This function may be useful if an agent function is shared between multiple agents
+     *
+     * @note The performance of this function is unlikely to be cheap unless used as part of an RTC agent function.
+     */
+    __device__ __forceinline__ static bool isAgent(const char* agent_name);
+    /**
+     * When passed an agent state, returns a boolean to confirm whether it matches the name of the agent input state of the current agent function
+     *
+     * This function may be useful if an agent function is shared between multiple agent states
+     *
+     * @note The performance of this function is unlikely to be cheap unless used as part of an RTC agent function (whereby it can be processed at compile time).
+     */
+    __device__ __forceinline__ static bool isState(const char* agent_state);
 };
 
 ////
@@ -398,6 +416,14 @@ template<typename T, unsigned int I, unsigned int J, unsigned int K, unsigned in
 __device__ __forceinline__ char* DeviceCurve::getEnvironmentMacroProperty(const char(&name)[M]) {
     return getVariablePtr<T, I*J*K*W>(name, Curve::variableHash("_macro_environment"), 0);
 }
+
+__device__ __forceinline__ bool DeviceCurve::isAgent(const char* agent_name) {
+    return util::dstrcmp(agent_name, sm()->agent_name) == 0;
+}
+__device__ __forceinline__ bool DeviceCurve::isState(const char* agent_state) {
+    return util::dstrcmp(agent_state, sm()->state_name) == 0;
+}
+
 }  // namespace curve
 }  // namespace detail
 }  // namespace flamegpu

include/flamegpu/runtime/detail/curve/curve_rtc.cuh
@@ -161,6 +161,16 @@ class CurveRTCHost {
      */
     void unregisterEnvMacroProperty(const char* propertyName);
     /**
+     * Register the name of the agent and it's state of the agent function
+     *
+     * Used by ReadOnlyDeviceAPI::isAgent() and ReadOnlyDeviceAPI::isState()
+     *
+     * @param agentName Name of the agent
+     * @param agentState Name of the agent's state
+     * @throws exception::UnknownInternalError If the agent has already been registered
+     */
+    void registerAgent(const std::string &agentName, const std::string &agentState);
+    /**
      * Set the filename tagged in the file (goes into a #line statement)
      * @param filename Name to be used for the file in compile errors
      * @note Do not include quotes
@@ -378,6 +388,14 @@ class CurveRTCHost {
      * <name, RTCVariableProperties>
      */
     std::map<std::string, RTCEnvMacroPropertyProperties> RTCEnvMacroProperties;
+    /**
+     * Agent name for ReadOnlyDeviceAPI::isAgent()
+     */
+    std::string agentName;
+    /**
+    * Agent name for ReadOnlyDeviceAPI::isState()
+    */
+    std::string agentState;
 };
 
 }  // namespace curve

include/flamegpu/simulation/detail/DeviceStrings.h
@@ -1,0 +1,43 @@
+#ifndef INCLUDE_FLAMEGPU_SIMULATION_DETAIL_DEVICESTRINGS_H_
+#define INCLUDE_FLAMEGPU_SIMULATION_DETAIL_DEVICESTRINGS_H_
+
+#include <string>
+#include <map>
+#include <sstream>
+
+namespace flamegpu {
+namespace detail {
+/**
+ * Utility for copying strings to device
+ */
+class DeviceStrings {
+ public:
+    /**
+     * Deallocates held device pointers
+     */
+    ~DeviceStrings();
+    /**
+     * Register a device string
+     */
+    void registerDeviceString(const std::string &host_string);
+    /**
+     * Returns a device pointer to the provided string
+     * @note If reallocation is required, earlier pointers may be invalidated
+     */
+    const char* getDeviceString(const std::string &host_string);
+
+ private:
+    std::stringstream host_stream;
+    // Cache stream in a string to reduce stream->string repeat conversion during sim execution
+    std::string host_buffer;
+    // Hold the offset into buffer for all registered strings
+    std::map<std::string, ptrdiff_t> offsets;
+    char* device_buffer = nullptr;
+    size_t device_buffer_occupied = 0;
+    size_t device_buffer_len = 0;
+};
+
+}  // namespace detail
+}  // namespace flamegpu
+
+#endif  // INCLUDE_FLAMEGPU_SIMULATION_DETAIL_DEVICESTRINGS_H_

commit bea73ac85d83c16dc02ab7907526592d5f538675

FLAMEGPU_SEATBELTS=OFF did not build
This bug was introduced by #1116

Closes #1137

include/flamegpu/simulation/CUDASimulation.h
@@ -562,15 +562,15 @@ class CUDASimulation : public Simulation {
          * Held here for tracking when to release cuda memory
          */
         std::shared_ptr<detail::EnvironmentManager> environment;
+        /**
+         * Provides copies of strings (agent/state names) on device
+         */
+        detail::DeviceStrings strings;
 #if !defined(FLAMEGPU_SEATBELTS) || FLAMEGPU_SEATBELTS
         /**
          * Provides buffers for device error checking
          */
         exception::DeviceExceptionManager exception;
-        /**
-         * Provides copies of strings (agent/state names) on device
-         */
-        detail::DeviceStrings strings;
 #endif
         explicit Singletons(const std::shared_ptr<detail::EnvironmentManager> &environment) : environment(environment) { }
     } * singletons;