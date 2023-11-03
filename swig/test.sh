a1f34e86779af20f86ada6189ecf66bbda4f9289
Robert Chisholm <R.Chisholm@sheffield.ac.uk>
2023-11-03 12:09:27 +0000

    wasn't meant to commit this

--- swig/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-[[ -z "$FLAMEGPU_CONDA_CUDA_ARCHITECTURES" ]] && build_arch="" || build_arch="-DCMAKE_CUDA_ARCHITECTURES=$FLAMEGPU_CONDA_CUDA_ARCHITECTURES"
-
-echo "${build_arch}"
-echo ${build_arch}
-echo $build_arch
