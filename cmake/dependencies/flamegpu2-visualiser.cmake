30789e0c05d199b9e361e0902b14534e68f9992e
Peter Heywood <peethwd@gmail.com>
2024-08-29 14:13:06 +0100

    CMake: Update visualisation repo hash to include MSVC, CMake & CI fixes

--- cmake/dependencies/flamegpu2-visualiser.cmake
+++ cmake/dependencies/flamegpu2-visualiser.cmake
@@ -11,7 +11,7 @@ if(POLICY CMP0169)
 endif()
 
 # Set the visualiser repo and tag to use unless overridden by the user.
-set(DEFAULT_FLAMEGPU_VISUALISATION_GIT_VERSION "flamegpu-2.0.0-rc.1")
+set(DEFAULT_FLAMEGPU_VISUALISATION_GIT_VERSION "3befb5964fc5e24c375e872ebbb5d903e878b5e0")
 set(DEFAULT_FLAMEGPU_VISUALISATION_REPOSITORY "https://github.com/FLAMEGPU/FLAMEGPU2-visualiser.git")
 
 # Set a VISUSLAITION_ROOT cache entry so it is available in the GUI to override the location if required
