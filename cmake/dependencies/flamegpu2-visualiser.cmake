74e1fae5782aec7d2d98c5539481fe178d036329
Peter Heywood <peethwd@gmail.com>
2024-11-19 11:55:09 +0000

    Update FLAMEGPU2-visualiser dependency

--- cmake/dependencies/flamegpu2-visualiser.cmake
+++ cmake/dependencies/flamegpu2-visualiser.cmake
@@ -11,7 +11,7 @@ if(POLICY CMP0169)
 endif()
 
 # Set the visualiser repo and tag to use unless overridden by the user.
-set(DEFAULT_FLAMEGPU_VISUALISATION_GIT_VERSION "dab83d071d0354f8bede5f16fc7bd24d89d453f8")
+set(DEFAULT_FLAMEGPU_VISUALISATION_GIT_VERSION "flamegpu-2.0.0-rc.2")
 set(DEFAULT_FLAMEGPU_VISUALISATION_REPOSITORY "https://github.com/FLAMEGPU/FLAMEGPU2-visualiser.git")
 
 # Set a VISUSLAITION_ROOT cache entry so it is available in the GUI to override the location if required
