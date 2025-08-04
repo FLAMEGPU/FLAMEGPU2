85242f2136d0973c491baca06791711941a0e8ad
Peter Heywood <peethwd@gmail.com>
2025-08-04 16:13:25 +0100

    Jitify2: pin the commit to the HEAD of the jitify2 branch on 2025-08-04 + a fix

--- cmake/dependencies/Jitify.cmake
+++ cmake/dependencies/Jitify.cmake
@@ -14,7 +14,7 @@ endif()
 FetchContent_Declare(
     jitify
     GIT_REPOSITORY https://github.com/NVIDIA/jitify.git
-    GIT_TAG        jitify2-preprocessing-overhaul
+    GIT_TAG        7d8fbc8dc5aa0609a84edaa860456a88f660cdd6  # jitify2 branch on 2025-08-04
     SOURCE_DIR     ${FETCHCONTENT_BASE_DIR}/jitify-src/jitify
     GIT_PROGRESS   ON
     # UPDATE_DISCONNECTED   ON
