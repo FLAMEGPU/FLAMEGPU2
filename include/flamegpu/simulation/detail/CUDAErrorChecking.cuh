b98aae63a4f9a1bf62ec0fb070ff57b602d0ddf5
Peter Heywood <peethwd@gmail.com>
2026-04-02 11:31:50 +0100

    Breaking change: Remove GPU error checking macros which polluted the global namespace

--- include/flamegpu/simulation/detail/CUDAErrorChecking.cuh
+++ include/flamegpu/simulation/detail/CUDAErrorChecking.cuh
@@ -14,12 +14,6 @@ namespace detail {
 
 /**
  * Error check function for safe CUDA API calling
- * Wrap any cuda runtime API calls with this macro to automatically check the returned cudaError_t
- */
-#define gpuErrchk(ans) { flamegpu::detail::gpuAssert((ans), __FILE__, __LINE__); }
-
-/**
- * Error check function for safe CUDA API calling
  * @param code CUDA Runtime API return code
  * @param file File where errorcode was reported (e.g. __FILE__)
  * @param line Line no where errorcode was reported (e.g. __LINE__)
@@ -31,11 +25,6 @@ inline void gpuAssert(cudaError_t code, const char *file, int line) {
     }
 }
 
-/**
- * Error check function for safe CUDA Driver API calling
- * Wrap any cuda drive API calls with this macro to automatically check the returned CUresult
- */
-#define gpuErrchkDriverAPI(ans) { flamegpu::detail::gpuAssert((ans), __FILE__, __LINE__); }
  /**
   * Error check function for safe CUDA API calling
   * @param code CUDA Driver API return code
@@ -50,12 +39,6 @@ inline void gpuAssert(CUresult code, const char* file, int line) {
     }
 }
 
-/**
- * Error check function for use after asynchronous methods
- * Call this macro function after async calls such as kernel launches to automatically check the latest error
- * In debug builds this will perform a synchronisation to catch any errors, in non-debug builds errors may be propagated.
- */
-#define gpuErrchkLaunch() { flamegpu::detail::gpuLaunchAssert(__FILE__, __LINE__); }
  /**
   * Error check function for checking for the most recent error
   * @param file File where errorcode was reported (e.g. __FILE__)
