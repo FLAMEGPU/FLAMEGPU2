5bb603f547f33d6b77427bb6384444a51cffe855
Peter Heywood <peethwd@gmail.com>
2026-04-28 13:58:48 +0100

    CMake: Do not validate or set CMAKE_CUDA_ARCHITECTURES
    Setting a default CMAKE_CUDA_ARCHITECTURES/CMAKE_HIP_ARCHITECTURES with optional CUDA or HIP language support is complex and non-standard for CMake Projects.
    
    Instead, rely on conventional use of CMake arguments and environment variables to provide a minimal value which should be usable by all users, while having to explicitly request all-major for hardware-optimised python wheels for distribution.
    
    Deprecates cmake/CUDAArchitectures.cmake, flamegpu_init_cuda_architectures and flamegpu_set_cuda_architectures
    
    No longer attemtpts to validate CMAKE_CUDA_ARCHITECTURES, which already had no impact due to changes in recent versions of CMake (causing errors before this validation could occur)
    
    Closes #1386
    
    Fixup: define variables needed in the enable_languages macro with the correct scope

--- cmake/CUDAArchitectures.cmake
+++ cmake/CUDAArchitectures.cmake
@@ -1,31 +1,32 @@
 #[[[
-# Deprecated module for handling CMAKE_CUDA_ARCHITECTURES with sane library-provided defaults. 
-# Use GPUArchitectures.cmake and flamegpu_init_gpu_architectures / flamegpu_set_gpu_architectures instead
+# Deprecated module which formerly handled CMAKE_CUDA_ARCHITECTURES with sane library-provided defaults. 
+# CMAKE_CUDA_ARCHITECTURES/CMAKE_HIP_ARCHITECTURES must now be handled by the user, else the cmake/compiler defaults will be used (SM_50 for CUDA 12, SM_75 for CUDA 13, 'native' for HIP (most of the time))
 #]]
 include_guard(GLOBAL)
 
-# Include the new version of this file
-include(${CMAKE_CURRENT_LIST_DIR}/GPUArchitectures.cmake)
+message(DEPRECATION
+    "  ${CMAKE_CURRENT_LIST_FILE} is deprecated and will be removed in a future release.\n"
+    "  flamegpu_init_cuda_architectures and flamegpu_set_cuda_architectures are no longer required.\n"
+    "  Please ensure you are setting CMAKE_CUDA_ARCHITECTURES/CMAKE_HIP_ARCHITECTURES appropriately for your GPUs.\n"
+)
 
-# This method is deprecated. Use flamegpu_set_gpu_architectures from GPUArchitectures.cmake instead
+# This method is deprecated and no longer has an impact, but has not yet been removed to avoid causing CMake configuration errors
 function(flamegpu_init_cuda_architectures)
     # Issue a deprecation warning
-    message(AUTHOR_WARNING
-        "  flamegpu_init_cuda_architectures is DEPRECATED.\n"
-        "  Please use flamegpu_init_gpu_architectures from GPUArchitectures.cmake instead.\n"
-        "  flamegpu_init_cuda_architectures will be removed in a future release."
+    message(DEPRECATION
+        "  flamegpu_init_cuda_architectures is deprecated and will be removed in a future release.\n"
+        "  Please remove the call to flamegpu_init_cuda_architectures and ensure you are setting CMAKE_CUDA_ARCHITECTURES/CMAKE_HIP_ARCHITECTURES appropriately for your GPUs."
     )
     # Forward all args to the generalised version of this function
     flamegpu_init_gpu_architectures(${ARGN})
 endfunction()
 
-# This method is deprecated. Use flamegpu_set_gpu_architectures from GPUArchitectures.cmake instead
+# This method is deprecated and no longer has an impact, but has not yet been removed to avoid causing CMake configuration errors
 function(flamegpu_set_cuda_architectures)
     # Issue a deprecation warning
-    message(AUTHOR_WARNING
-        "  flamegpu_set_cuda_architectures is DEPRECATED.\n"
-        "  Please use flamegpu_set_gpu_architectures from GPUArchitectures.cmake instead.\n"
-        "  flamegpu_set_cuda_architectures will be removed in a future release."
+    message(DEPRECATION
+        "  flamegpu_set_cuda_architectures is deprecated and will be removed in a future release.\n"
+        "  Please remove the call to flamegpu_set_cuda_architectures and ensure you are setting CMAKE_CUDA_ARCHITECTURES/CMAKE_HIP_ARCHITECTURES appropriately for your GPUs."
     )
     # Forward all args to the generalised version of this function
     flamegpu_set_gpu_architectures(${ARGN})
